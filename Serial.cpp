75d0c3d1d051df44a49b2c57a9b2d5ecf8427516
(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧* <heathlydallows@Jennifer.local>
2018-03-06 23:40:52 -0800

    testing serial

--- Serial.cpp
+++ Serial.cpp
@@ -5,12 +5,33 @@
 // Sources of Help: TODO
 // Due: March 10th, 2018 at 11:59 PM
 
+/*
+traverse the list for i= 0 to n-1 elements
+{
+  check for sign of A[abs(A[i])] ;
+  if positive then
+     make it negative by   A[abs(A[i])]=-A[abs(A[i])];
+  else  // i.e., A[abs(A[i])] is negative
+     this   element (ith element of list) is a repetition
+}
+*/
+
 #ifndef __SERIAL_CPP__
 #define __SERIAL_CPP__
 
 #include "Serial.hpp"
+#include <iostream>    
+#include <stdlib.h> 
 
 int serial(std::vector<int> nums) {
+	for(auto it = nums.begin(); it != nums.end(); it++) { 
+		if(nums[*it->first] >= 0){
+			arr[*it->first] = arr[*it->first] * -1;
+		}
+		else{
+			return arr[*it->first];
+		}
+	}
   return 0;
 }
 #endif
