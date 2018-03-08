3df3a43978cbc73fa6883f8dade49f8b8b541266
(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧* <heathlydallows@Jennifer.local>
2018-03-07 18:37:32 -0800

    working on building::floodfill

--- Buildings.cpp
+++ Buildings.cpp
@@ -8,9 +8,42 @@
 #ifndef __BUILDINGS_CPP__
 #define __BUILDINGS_CPP__
 
+#include <queue>
+#include "Interval.hpp"
 #include "Buildings.hpp"
 
+/* interval: start == rows, end == cols
+
+*/
+
+static void floodfill(TwoD_Array<int> survey, Interval node, int target, int replace){
+	if (target == replace){
+		return;
+	}
+	if (survey.at.(node->start, node->end) != target){
+		return;
+	}
+	std::queue<Interval> q;
+	q.push(node);
+	while(!q.empty()){
+		Interval popped = q.top();
+		q.pop();
+		int w = survey.at(popped->start, popped->end);
+		int e = survey.at(popped->start, popped->end);
+		for(int i = popped->end ; i< survey.getNumCols(); i++){
+
+		}
+		for(int i = popped->end; i >= 0; i--){
+			
+		}
+	}
+
+
+}
+
 int buildings(TwoD_Array<int> survey) {
+	arr = new TwoD_Array<int>(survey.getNumRows(), survey.getNumCols());
+
   return 0;
 }
 #endif
